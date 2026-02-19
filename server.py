c01398e5dc43264202b5e246d83247f6239605b5
Rubens Carneiro <rubenscarneiro@protonmail.com>
2026-02-19 03:36:00 -0500

    R-01 Personalization (#1)
    * R-01 INIT COMMIT - Disable files for testing
    
    * R-01 Update index removing unnecessary code
    
    * R-01 Remove imports for testing
    
    * R-01 Fix UI update
    
    * R-01 Set trusted require.js
    
    * R-01 Remove archive folder
    
    * R-01 Update point page
    
    * R-01 Update readme
    
    * R-01 HTTPS server for local network usage
    
    * R-01 Style changes
    
    * R-01 Remove extra classes
    
    * R-01 Remove automatic TLE update
    
    * R-01 Remove CNAME settings

---
+++ server.py
@@ -0,0 +1,38 @@
+import os
+import http.server
+import ssl
+import socketserver
+import argparse
+
+parser = argparse.ArgumentParser(description="HTTPSServer \n Usage: python3 server.py 8987")
+parser.add_argument("port", type=int, default=443, nargs="?", help="Port Number, default is 8987")
+args = parser.parse_args()
+CERT_PATH = "./cert/localhost.crt"
+KEY_PATH = "./cert/localhost.key"
+PEM_PATH = "./cert/localhost.pem"
+
+def create_https_server(cert_path,key_path,port=8987):
+    if (os.geteuid() != 0 and port <=1024):
+        exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
+
+    server_address = ('0.0.0.0', port)
+
+    try:
+        print('Creating context...')
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        context.load_cert_chain(cert_path, key_path)
+
+        print('Creating context...')
+        with socketserver.TCPServer(server_address, http.server.SimpleHTTPRequestHandler) as httpd:
+            with context.wrap_socket(httpd.socket, server_side=True) as ssock:
+                httpd.socket = ssock
+                
+                print(f"Serving securely on https://localhost:{port}")
+                httpd.serve_forever()
+    except Exception as e:
+        print(e)
+        exit("[ERR] Port %d has been taken."%(port))
+
+    
+if __name__ == "__main__":
+    create_https_server(CERT_PATH,KEY_PATH,args.port)
