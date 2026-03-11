d7bdf3505c11f6c4859d6bb0e976162d791bdf7b
Rubens Carneiro <rcarneiro@meetsoci.com>
2026-03-10 23:27:26 -0400

    E-02 Create starter script

---
+++ start_server.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail;
+
+PORT="${1:-${SAT_TRACKER_PORT:-8987}}";
+
+export HOST_UID="$(id -u)";
+export HOST_GID="$(id -g)";
+export SAT_TRACKER_PORT="$PORT"-;
+
+docker compose up -d;
